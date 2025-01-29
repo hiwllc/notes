b641b525e8044e45b5beef99187e925e763387af
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-01-29 19:47:40 -0300

    feat(notes): add empty states for notes

--- app/(app)/me/page.tsx
+++ app/(app)/me/page.tsx
@@ -1,6 +1,7 @@
 import { NoteCard } from "@/components/note";
 import { prisma } from "@/lib/prisma";
 import { requireAuthenticatedUser } from "@/lib/utils";
+import Link from "next/link";
 
 async function getAllNotesFromUser(user: string) {
 	return prisma.note.findMany({
@@ -28,6 +29,20 @@ export default async function ProfilePage() {
 	return (
 		<section className="space-y-10">
 			<h2 className="text-xl font-black text-foreground/80">Suas Notas</h2>
+
+			{notes.length <= 0 ? (
+				<div className="gap-6 pb-24 lg:pb-6">
+					<h3 className="text-md font-medium text-foreground/80">
+						Você ainda não criou nenhuma nota...
+					</h3>
+					<p className="text-sm text-muted-foreground">
+						Ainda não temos nenhuma nota cadastrada, que tal{" "}
+						<Link href="/create" className="text-blue-700 underline">
+							criar sua primeira nota?
+						</Link>
+					</p>
+				</div>
+			) : null}
 
 			<div className="columns-[280px] gap-6 md:columns-xs lg:columns-md pb-6">
 				{notes.map((note) => (
--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import { NoteCard } from "@/components/note";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
 async function getAllNotes(user?: string) {
 	return prisma.note.findMany({
@@ -28,6 +29,20 @@ export default async function Home() {
 	return (
 		<section className="space-y-10">
 			<h2 className="text-xl font-black text-foreground/80">Últimas Notas</h2>
+
+			{notes.length <= 0 ? (
+				<div className="gap-6 pb-24 lg:pb-6">
+					<h3 className="text-md font-medium text-foreground/80">
+						Nenhuma nota por aqui ainda.
+					</h3>
+					<p className="text-sm text-muted-foreground">
+						Ainda não temos nenhuma nota cadastrada, que tal{" "}
+						<Link href="/create" className="text-blue-700 underline">
+							criar sua primeira nota?
+						</Link>
+					</p>
+				</div>
+			) : null}
 
 			<div className="columns-[280px] gap-6 md:columns-xs lg:columns-md pb-24 lg:pb-6">
 				{notes.map((note) => (
