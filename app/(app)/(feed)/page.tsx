31d0da423b1669609013db5a1b9c3ed3c8e10a89
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-01-30 22:56:39 -0300

    chore: spaces auto adjusts

--- app/(app)/(feed)/page.tsx
+++ app/(app)/(feed)/page.tsx
@@ -1,73 +1,65 @@
 import { NoteCard } from "@/components/note";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
 async function getAllNotes(user?: string) {
-	return prisma.note.findMany({
-		where: {
-			OR: [{ visibility: "PUBLIC", status: "PUBLISHED" }, { userId: user }],
-		},
-		select: {
-			id: true,
-			title: true,
-			content: true,
-			createdAt: true,
-			status: true,
-			visibility: true,
-			user: {
-				select: {
-					id: true,
-					name: true,
-					email: true,
-				},
-			},
-		},
-		orderBy: {
-			createdAt: "desc",
-		},
-	});
+  return prisma.note.findMany({
+    where: {
+      OR: [{ visibility: "PUBLIC", status: "PUBLISHED" }, { userId: user }],
+    },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      status: true,
+      visibility: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 }
 
 export default async function Home() {
-	const session = await auth();
-	const notes = await getAllNotes(session?.user?.id);
+  const session = await auth();
+  const notes = await getAllNotes(session?.user?.id);
 
-	return (
-		<section className="space-y-10">
-			<h2 className="text-xl font-black text-foreground/80">Últimas Notas</h2>
+  return (
+    <section className="space-y-10">
+      <h2 className="text-xl font-black text-foreground/80">Últimas Notas</h2>
 
-			{notes.length <= 0 ? (
-				<div className="gap-6 pb-24 lg:pb-6">
-					<h3 className="text-md font-medium text-foreground/80">
-						Nenhuma nota por aqui ainda.
-					</h3>
-					<p className="text-sm text-muted-foreground">
-						Ainda não temos nenhuma nota cadastrada, que tal{" "}
-						<Link href="/create" className="text-blue-700 underline">
-							criar sua primeira nota?
-						</Link>
-					</p>
-				</div>
-			) : null}
+      {notes.length <= 0 ? (
+        <div className="gap-6 pb-24 lg:pb-6">
+          <h3 className="text-md font-medium text-foreground/80">
+            Nenhuma nota por aqui ainda.
+          </h3>
 
-			<div className="columns-[280px] gap-6 md:columns-xs lg:columns-md pb-24 lg:pb-6">
-				{notes.map((note) => (
-					<NoteCard
-						key={note.id}
-						note={note}
-						isOwner={session?.user?.id === note.user.id}
-					/>
-				))}
-			</div>
-		</section>
-	);
+          <p className="text-sm text-muted-foreground">
+            Ainda não temos nenhuma nota cadastrada, que tal{" "}
+            <Link href="/create" className="text-blue-700 underline">
+              criar sua primeira nota?
+            </Link>
+          </p>
+        </div>
+      ) : null}
+      <div className="columns-[280px] gap-6 md:columns-xs lg:columns-md pb-24 lg:pb-6">
+        {notes.map((note) => (
+          <NoteCard
+            key={note.id}
+            note={note}
+            isOwner={session?.user?.id === note.user.id}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,8 @@
 import {
-	MoonIcon,
-	MoveRightIcon,
-	NotebookTextIcon,
-	SearchIcon,
-	SunIcon,
+  MoonIcon,
+  MoveRightIcon,
+  NotebookTextIcon,
+  SunIcon,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/user-card";
@@ -14,88 +13,77 @@ import { prisma } from "@/lib/prisma";
 import { getTheme, setTheme } from "@/actions/theme";
 
 async function getUserNotesCount(user?: string) {
-	if (!user) {
-		return 0;
-	}
+  if (!user) {
+    return 0;
+  }
 
-	return prisma.note.count({ where: { userId: user } });
+  return prisma.note.count({ where: { userId: user } });
 }
 
 export default async function AppLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
   const theme = await getTheme();
-	const session = await auth();
+  const session = await auth();
   const notes = await getUserNotesCount(session?.user?.id);
 
-	return (
-		<main className="w-full min-h-dvh">
-			<header className="sticky top-0 bg-background z-20 lg:h-20">
-				<div className="container grid grid-rows-2 md:grid-rows-1 py-4 gap-2 md:grid-cols-[1fr_232px]">
-					<h1 className="text-sm font-medium row-start-1">
-						<Link href="/" className="flex items-center gap-2">
-							<NotebookTextIcon className="size-4" />
-							overnote
-						</Link>
-					</h1>
+  return (
+    <main className="w-full min-h-dvh">
+      <header className="sticky top-0 bg-background z-20 lg:h-20">
+        <div className="container grid grid-rows-2 md:grid-rows-1 py-4 gap-2 md:grid-cols-[1fr_232px]">
+          <h1 className="text-sm font-medium row-start-1">
+            <Link href="/" className="flex items-center gap-2">
+              <NotebookTextIcon className="size-4" />
+              overnote
+            </Link>
+          </h1>
 
-					<div className="flex items-center p-1 bg-background rounded-md border focus-within:ring-foreground focus-within:ring-offset-2 col-span-2 md:row-start-1">
-						<input
-							className="w-full flex-1 h-6 px-2 bg-transparent focus:outline-none text-sm"
-							placeholder="Digite para pesquisar..."
-						/>
+          <form action={setTheme} className="row-start-1 ml-auto">
+            <input
+              name="theme"
+              value={theme === "light" ? "dark" : "light"}
+              hidden
+              readOnly
+            />
 
-						<Button size="icon" className="size-6 p-0" variant="ghost">
-							<SearchIcon className="size-4" />
-						</Button>
-					</div>
+            <Button className="size-8 p-0" size="icon" variant="outline">
+              <SunIcon className="size-4 scale-100 rotate-0 dark:scale-0 dark:-rotate-90 transition-transform" />
+              <MoonIcon className="size-4 absolute scale-0 rotate-90 dark:scale-100 dark:rotate-0 transition-transform" />
+              <span className="sr-only">Alternar Tema</span>
+            </Button>
+          </form>
+        </div>
+      </header>
 
-					<form action={setTheme} className="row-start-1 ml-auto">
-						<input
-							name="theme"
-							value={theme === "light" ? "dark" : "light"}
-							hidden
-							readOnly
-						/>
+      <section className="container grid gap-10 h-[calc(100dvh-160px)] py-10 lg:grid-cols-[260px_1fr] lg:h-[calc(100dvh-140px)]">
+        <aside className="gap-2 sticky top-0">
+          {!session ? (
+            <div className="fixed lg:static bottom-0 left-0 bg-background lg:bg-muted-foreground/5 border-t lg:border-0 text-center lg:text-left rounded-t-md lg:rounded-md p-4 space-y-3 w-full">
+              <p className="text-sm font-medium text-foreground/80">
+                Faça login ou crie sua conta para criar suas notas.
+              </p>
 
-						<Button className="size-8 p-0" size="icon" variant="outline">
-							<SunIcon className="size-4 scale-100 rotate-0 dark:scale-0 dark:-rotate-90 transition-transform" />
-							<MoonIcon className="size-4 absolute scale-0 rotate-90 dark:scale-100 dark:rotate-0 transition-transform" />
-							<span className="sr-only">Alternar Tema</span>
-						</Button>
-					</form>
-				</div>
-			</header>
+              <Button size="sm" className="group" asChild>
+                <Link href="/login">
+                  Login{" "}
+                  <MoveRightIcon className="size-4 group-hover:translate-x-1 transition-transform" />
+                </Link>
+              </Button>
+            </div>
+          ) : null}
 
-			<section className="container grid gap-10 h-[calc(100dvh-160px)] py-10 lg:grid-cols-[260px_1fr] lg:h-[calc(100dvh-140px)]">
-				<aside className="gap-2 sticky top-0">
-					{!session ? (
-						<div className="fixed lg:static bottom-0 left-0 bg-background lg:bg-muted-foreground/5 border-t lg:border-0 text-center lg:text-left rounded-t-md lg:rounded-md p-4 space-y-3 w-full">
-							<p className="text-sm font-medium text-foreground/80">
-								Faça login ou crie sua conta para criar suas notas.
-							</p>
+          {session?.user ? (
+            <>
+              <UserButton user={session.user} notes={notes} />
+              <NavMain />
+            </>
+          ) : null}
+        </aside>
 
-							<Button size="sm" className="group" asChild>
-								<Link href="/login">
-									Login{" "}
-									<MoveRightIcon className="size-4 group-hover:translate-x-1 transition-transform" />
-								</Link>
-							</Button>
-						</div>
-					) : null}
-
-					{session?.user ? (
-						<>
-							<UserButton user={session.user} notes={notes} />
-							<NavMain />
-						</>
-					) : null}
-				</aside>
-
-				{children}
-			</section>
-		</main>
-	);
+        {children}
+      </section>
+    </main>
+  );
 }
